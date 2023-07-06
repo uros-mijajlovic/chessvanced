49204c74bf8907710b683c48ad3b8a04619bcc63
unknown <t-urosm@microsoft.com>
2023-07-06 18:21:17 +0200

    fixed responsiveness

--- src/connectStockfish.js
+++ src/connectStockfish.js
@@ -90,7 +90,21 @@ stockfishOrchestratorInst.analysisOrchestrator=analsysOrchestratorInst;
 
 //INICIALIZATION
 
+const numDivs = 5;
 
+// Create a div with a unique ID and attach a click event listener
+for (let i = 0; i < numDivs; i++) {
+  const div = document.createElement('div');
+  div.id = `div${i + 1}`;
+  div.textContent = `Div ${i + 1}`;
+  div.addEventListener('click', handleClick);
+  document.body.appendChild(div);
+}
+
+// Event handler for div click
+function handleClick(event) {
+  console.log(event.target.id);
+}
 
 
 playerControllerInst.setPgn(pgn_string_1);
