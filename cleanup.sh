e986c7d413dda724d0b0e6c9ba8ed3b46b08ee04
unknown <t-urosm@microsoft.com>
2023-08-24 14:28:18 +0200

    added cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define an array of files and directories to exclude
+declare -a exclude=("cleanup.sh" "index.html" "distWebpack" "dependencies" "assets" "img" "styles" "favicon.ico")
+
+# Loop through all items in the current directory
+for item in *; do
+    # Check if the item is in the exclusion list
+    if [[ " ${exclude[@]} " =~ " ${item} " ]]; then
+        echo "Skipping: $item"
+    else
+        # Check if the item is a file and not a directory
+        if [ -f "$item" ]; then
+            # Delete the file
+            rm "$item"
+            echo "Deleted: $item"
+        elif [ -d "$item" ]; then
+            # Check if the item is a directory and not in the exclusion list
+            echo "Skipping directory: $item"
+        else
+            # This is an unknown item (not a file or directory)
+            echo "Unknown item: $item"
+        fi
+    fi
+done
